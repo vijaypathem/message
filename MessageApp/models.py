b7051810b21448397fae07a64209070980f8eb66
vijay pathem <vijaypathem4@gmail.com>
2024-03-10 02:39:39 +0530

    First Commit

---
+++ MessageApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# models.py
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Message(models.Model):
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SecretMessages.wsgi
